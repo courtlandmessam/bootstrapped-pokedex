8a14284fc37c09f381ba0eadc38e7f65ebcdcf4d
courtlandmessam <messamcourtland@gmail.com>
2020-02-01 15:54:30 -0500

    Checked to see if pokemon is valid

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 window.addEventListener('load', ()=> {
 
-  let api = "https://pokeapi.co/api/v2/pokemon/butterfree";
 
-  fetch(api)
-    .then(response => {
-      console.log(response.json());
-    })
+     var btn = document.getElementById("search-pokemon");
+     btn.onclick = function() { getPokemon(); }
 
 
+    function getPokemon() {
+      var pokemon = document.getElementById('pokemon-request').value;
+
+
+      let api = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+      fetch(api)
+        .then(response => {
+
+          //Checks to see if input is a valid pokemon
+          if(!response.ok) {
+            document.getElementById('pokemon-name').innerHTML = "Not a valid Pokemon";
+          } else {
+            return response.json();
+          }
+
+        })
+        .then(data => {
+          document.getElementById('pokemon-name').innerHTML = data.name;
+          document.getElementById("pokemon-image").src = data.sprites.front_female;
+          console.log(data.name);
+        })
+    }
 
 
 
